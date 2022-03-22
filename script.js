c51784b1014c9ea67560db9571b92f3635146b6e
Fluoclip <59053824+Fluoclip@users.noreply.github.com>
2022-03-22 11:06:18 +0100

    CSS update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
 
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
-document.getElementById("current_time").innerHTML = dateTime;
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)
+    +'-'+today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() +
+    ":" + today.getSeconds();
+  var dateTime = date+' '+time;
+  document.getElementById("current_time").innerHTML = dateTime;
